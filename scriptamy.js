059ceb71e3063be8c5869cd7a3b557bfda8e1df4
mazal <mazalwebdesign@gmail.com>
2021-06-09 15:31:55 +0300

    adding script particles

---
+++ scriptamy.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background'
+    });
+  };
+
+var particles = Particles.init({
+    selector: ".background",
+    color: ["#90F7EC", "#26c8c8", "#e15f5f"],
+    connectParticles: true
+  })
