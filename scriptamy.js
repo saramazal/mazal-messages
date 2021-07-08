395763190f4d3e8934e44e670141203bd4d98f25
saramazal <32996161+saramazal@users.noreply.github.com>
2021-07-08 10:48:07 +0300

    Update scriptamy.js

--- scriptamy.js
+++ scriptamy.js
@@ -7,13 +7,12 @@ window.onload = function() {
 var particles = Particles.init({
     selector: ".background",
     color: ["#ffffff", "#26c8c8", "#e15f5f"],
-   
-connectParticles: true,
+   connectParticles: true,
   responsive: [
     {
       breakpoint: 375,
       options: {
-        color: "#e15f5f,
+        color: "#e15f5f",
         maxParticles: 43,
         connectParticles: false
       }
